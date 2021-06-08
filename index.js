73936f21845b5190a60603c14a0244b3a8f784bc
Mary Grishchuk <mary.grishchuk1@gmail.com>
2021-06-09 00:11:23 +0300

    added console.logs to log info about sent emails

--- index.js
+++ index.js
@@ -29,40 +29,46 @@ app.post('/sendMessage', async (req, res) => {
     let {name, email, messageText} = req.body
     try {
         //notifying sender that the message is received:
-        let senderNotifiedInfo = await transporter.sendMail({
-            from: `"no-reply" <mary.grishchuk1@gmail.com>`, // sender address
-            to: `${email}`, // list of receivers
-            subject: "Thank you for your message to Mary Grishchuk", // Subject line
-            html: `<p>Dear ${name},</p>
+        await transporter.sendMail({
+                from: `"no-reply" <mary.grishchuk1@gmail.com>`, // sender address
+                to: `${email}`, // list of receivers
+                subject: "Thank you for your message to Mary Grishchuk", // Subject line
+                html: `<p>Dear ${name},</p>
                    <p><span>Thank you for your message to Mary Grishchuk. </span>
                    She will get in touch with you as soon as she reads it.</p>
                    <p>Have a great day!</p>
                    <p><span>** Please kindly note: Do not reply to this email. </span>
                    This email is sent from an unattended mailbox. Replies will not be read.</p>`, // html body
-            dsn: {
-                id: 'message_id',
-                return: 'headers',
-                notify: ['failure', 'delay'],
-                recipient: 'mary.grishchuk1@gmail.com'
-            }
-        })
+                dsn: {
+                    id: 'message_id',
+                    return: 'headers',
+                    notify: ['failure', 'delay'],
+                    recipient: 'mary.grishchuk1@gmail.com'
+                }
+            },
+            (err, info) => {
+                console.log('senderNotifiedInfo.envelope: ' + info.envelope);
+                console.log('senderNotifiedInfo.messageId: ' + info.messageId);
+            })
         //sending form data to myself including information about notifying the sender:
-        let mailSentToMeInfo = await transporter.sendMail({
-            from: `"${name}" <${email}>`, // sender address
-            to: "mary.grishchuk1@gmail.com", // list of receivers
-            subject: "New message from portfolio 'Contact me' form", // Subject line
-            html: `<div><b>Name:</b> ${name}</div>
+        await transporter.sendMail({
+                from: `"${name}" <${email}>`, // sender address
+                to: "mary.grishchuk1@gmail.com", // list of receivers
+                subject: "New message from portfolio 'Contact me' form", // Subject line
+                html: `<div><b>Name:</b> ${name}</div>
                    <div><b>Email:</b> ${email}</div>
-                   <div><b>Message:</b> ${messageText}</div>
-                   <div><b>Information about notifying the sender:</b> ${senderNotifiedInfo.envelope}</div>`,//html body
-            dsn: {
-                id: 'message_id',
-                return: 'headers',
-                notify: ['failure', 'delay'],
-                recipient: 'mary.grishchuk1@gmail.com'
-            }
-        })
-        console.log(mailSentToMeInfo)
+                   <div><b>Message:</b> ${messageText}</div>`,//html body
+                dsn: {
+                    id: 'message_id',
+                    return: 'headers',
+                    notify: ['failure', 'delay'],
+                    recipient: 'mary.grishchuk1@gmail.com'
+                }
+            },
+            (err, info) => {
+                console.log('mailSentToMeInfo.envelope: ' + info.envelope);
+                console.log('mailSentToMeInfo.messageId: ' + info.messageId);
+            })
         res.send('ok')
     } catch (error) {
         res.send('Failure to send. error: ' + error)
